1104469fd84f80705d9dc7dbce6f8a08db88c691
Dominick <dominick.tucker1@gmail.com>
2023-04-24 08:56:22 -0700

    finished challenge 12

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
+
+let posts = [];
+let test = console.log(posts);
 
 app.set('view engine', 'ejs');
 
@@ -19,7 +23,8 @@ app.use(express.static("public"));
 
 app.get('/', function(req, res) {
   res.render("home", {
-    loremIpsumText: homeStartingContent
+    loremIpsumText: homeStartingContent,
+    homePosts: posts
   });
 });
 
@@ -51,19 +56,15 @@ app.post("/compose", function(req, res){
     post: req.body.submission  //references the post textarea
   }
 
-  console.log(postObject.title) //when user enters text into Title field will console log it into Hyper
-  console.log(postObject.post); //when user enters text into Post field will console log it into Hyper
-  console.log("nick code beatdown")
-
-
+  posts.push(postObject);
+ 
+  res.redirect("/")
 });
-
-
-
-
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
+  // console.log(postObject.title) //when user enters text into Title field will console log it into Hyper
+  // console.log(postObject.post); //when user enters text into Post field will console log it into Hyper
